2c7a7a9fcbbd9699c6ba83321b7e95b9be5a521a
Pankaj Singh <pankajsingh@aaraglobal.com>
2024-06-19 00:56:07 +0530

    next first

---
+++ src/component/Header.js
@@ -0,0 +1,44 @@
+// components/Header.js
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white shadow-md">
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        <div className="text-xl font-bold">Your Logo</div>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex items-center">
+          <button className="text-white focus:outline-none">
+            <FontAwesomeIcon icon={faBars} className="text-2xl" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
